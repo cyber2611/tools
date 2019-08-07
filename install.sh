d1cf90fe882882387872029a72439e4d54ddfd2d
cyber2611 <52934500+cyber2611@users.noreply.github.com>
2019-08-07 11:14:20 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,57 @@
+clear
+figlet Brother | lolcat
+echo "=====================================================
+
+╔══╗╔═╗╔═╗╔══╗╔╗╔╗╔═╗╔═╗╔╗─╔╗╔══╗╔═╗╔══╗╔═╗╔═╗╔═╦╗
+║╔╗║║╬║║║║╚╗╔╝║╚╝║║╦╝║╬║║╚╦╝║╚║║╝║╔╝╚╗╔╝║║║║╬║╚╗║║
+║╔╗║║╗╣║║║─║║─║╔╗║║╩╗║╗╣╚╗║╔╝╔║║╗║╚╗─║║─║║║║╗╣╔╩╗║
+╚══╝╚╩╝╚═╝─╚╝─╚╝╚╝╚═╝╚╩╝─╚═╝─╚══╝╚═╝─╚╝─╚═╝╚╩╝╚══╝
+╔════•ೋೋ•══════════════════════•
+ Author :Yoga Wira
+ Github :cyber2611
+ Fb     :Yoga Wira  
+╚════•ೋೋ•══════════════════════•
+   
+======================================================" | lolcat
+
+sleep 2
+
+echo "[1] Dark
+[2] BugHunter
+[3] Ddos
+[4] exit" | lolcat
+echo "[*] Pilih Nomor : " | lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor]
+then
+clear
+pkg install git
+git clone https://github.com/cyber2611/dark3
+ls
+cd dark3
+ls
+python2 coding2.py
+fi
+
+if [ $nomor = 2 ] || [ $nomor]
+then
+clear
+pkg install git
+git clone https://github.com/MRA27/Bughunter
+ls
+cd Bughunter
+ls
+python2 bughunter.py
+fi
+
+if [ $nomor = 3 ] || [ $nomor]
+then
+clear
+pkg install git
+git clone https://github.com/MRA27/DDOS
+ls
+cd DDOS
+ls
+python2 run.py
+fi
